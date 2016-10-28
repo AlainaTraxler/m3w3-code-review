cc8aa8fef25d7b62ca842ea181b10cfa34a0e2bb
Alaina Traxler <eloavox@gmail.com>
2016-10-28 11:39:01 -0700

    add answer-tile and set up one-to-many for questions/answers.

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   user: DS.attr(),
   question: DS.attr(),
   further: DS.attr(),
-  date: DS.attr()
+  date: DS.attr(),
+  answers: DS.hasMany('answer', { async: true })
 });
