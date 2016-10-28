edec07b6300906acc700d2b10a4c349eddeec728
Alaina Traxler <eloavox@gmail.com>
2016-10-28 10:18:41 -0700

    add question-new component and styling.

--- app/components/question-new.js
+++ app/components/question-new.js
@@ -1,1 +1,14 @@
+import Ember from 'ember';
 
+export default Ember.Component.extend({
+  inCreate: false,
+  actions:{
+    toggleCreate(){
+      if(this.inCreate){
+        this.set("inCreate", false);
+      } else {
+        this.set("inCreate", true);
+      }
+    },
+  }
+});
