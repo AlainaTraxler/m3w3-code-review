258207cf11960aaba031c7ed5cd01961ae95eb3d
Alaina Traxler <eloavox@gmail.com>
2016-11-04 08:38:11 -0700

    add helper for formatting dates and timestamps.

---
+++ app/helpers/time-format.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function timeFormat(_input) {
+  _input = _input[0].split(" ");
+  console.log(_input);
+  _input = `${_input[0]} ${_input[1]} ${_input[2]},  ${_input[3]} @  ${_input[4]} ${_input[6]}`;
+
+  return _input;
+}
+
+export default Ember.Helper.helper(timeFormat);
